37039750080e762421719b3f797685d243105b61
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-02-11 22:37:17 -0300

    Update certificate upload paths to include PRINT directory

--- src/shared/infra/repositories/certificate_repository_s3.ts
+++ src/shared/infra/repositories/certificate_repository_s3.ts
@@ -32,7 +32,7 @@ export async function saveCertificate(
     const command = new PutObjectCommand(params);
     const data = await s3.send(command);
     console.log("Certificate uploaded successfully. Data:", data);
-    const certificateUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/${userId}-${eventId}-certificado.pdf`;
+    const certificateUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/CERTIFICADOS-PRINT/${userId}-${eventId}-certificado.pdf`;
     return certificateUrl;
   } catch (error: any) {
     console.error("Error uploading certificate to S3:", error);
@@ -50,7 +50,7 @@ export async function saveCertificateExternal(
 
   const params = {
     Bucket: `${process.env.BUCKET_NAME}`,
-    Key: `certificados-externos/${email}-${eventId}-certificado.pdf`,
+    Key: `CERTIFICADOS-EXTERNOS-PRINT/${email}-${eventId}-certificado.pdf`,
     Body: certificatePdf,
   };
   console.log(params.Key);
@@ -59,7 +59,7 @@ export async function saveCertificateExternal(
     const command = new PutObjectCommand(params);
     const data = await s3.send(command);
     console.log("Certificate uploaded successfully. Data:", data);
-    const certificateUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/${email}-${eventId}-certificado.pdf`;
+    const certificateUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/CERTIFICADOS-EXTERNOS-PRINT/${email}-${eventId}-certificado.pdf`;
     return certificateUrl;
   } catch (error: any) {
     console.error("Error uploading certificate to S3:", error);
